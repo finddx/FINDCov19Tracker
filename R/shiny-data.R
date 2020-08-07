f4db9788ddb0967325543befd59d813f298b2b4e
Christoph Sax <christoph.sax@gmail.com>
2020-08-07 12:50:23 +0200

    move data preparation from app to preparation repo

--- R/shiny-data.R
+++ R/shiny-data.R
@@ -3,7 +3,11 @@
 #'   files from `processed/` directory.
 #'   Writes `processed/data_shiny.csv`.
 #' @export
+#' @import dplyr tibble
 create_shiny_data <- function() {
+
+  # read and cobine data -------------------------------------------------------
+
   cv_cases <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/processed/coronavirus_cases.csv",
     col_types = readr::cols()
   )
@@ -15,7 +19,7 @@ create_shiny_data <- function() {
   pop <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/raw/UN_populations_2020.csv",
     col_types = readr::cols()
   ) %>%
-    dplyr::mutate(country = dplyr::recode(.data$country,
+    mutate(country = recode(.data$country,
       "Republic of Korea" = "RepublicofKorea",
       "Congo (Brazzaville)" = "Congo",
       # "Republic of the Congo" = Congo
@@ -80,8 +84,8 @@ create_shiny_data <- function() {
 
   iso_country <-
     countrycode::codelist %>%
-    tibble::as_tibble() %>%
-    dplyr::select(
+    as_tibble() %>%
+    select(
       regex = country.name.en.regex, country = iso2c
     )
 
@@ -95,11 +99,12 @@ create_shiny_data <- function() {
           countries: {countries_without_population}.", wrap = TRUE)
   }
 
-  data <- cv_cases %>%
-    dplyr::left_join(cv_tests, by = c("country" = "jhu_ID", "date" = "date")) %>%
-    dplyr::left_join(pop) %>%
-    dplyr::mutate(pop_100k = population / 100000) %>%
-    dplyr::select(
+  data_combined_raw <-
+    cv_cases %>%
+    left_join(cv_tests, by = c("country" = "jhu_ID", "date" = "date")) %>%
+    left_join(pop) %>%
+    mutate(pop_100k = population / 100000) %>%
+    select(
       name = country,
       date,
       cases,
@@ -109,12 +114,9 @@ create_shiny_data <- function() {
       tests = tests_cumulative,
       new_tests,
       pop_100k
-    )
-
-  data_shiny <-
-    data %>%
+    ) %>%
     fuzzyjoin::regex_left_join(iso_country, by = c("name" = "regex"), ignore_case = TRUE) %>%
-    dplyr::mutate(country_iso = dplyr::case_when(
+    mutate(country = case_when(
       name == "Kosovo" ~ "XK",
       name == "SouthAfrica" ~ "ZA",
       name == "CentralAfricanRepublic" ~ "CF",
@@ -126,16 +128,128 @@ create_shiny_data <- function() {
     # drop
     # 1 Diamond Princess Cruise Ship
     # 2 MS Zaandam
-    dplyr::select(-regex, -country) %>%
-    dplyr::rename(name_full = name) %>%
-    dplyr::select(name_full, country_iso, dplyr::everything())
+    select(-regex) %>%
+    select(name, country, everything())
 
   # filter data which no iso_code match
-  data_shiny_filtered <- data_shiny %>%
-    dplyr::filter(!is.na(country_iso))
+  data_combined <- data_combined_raw %>%
+    filter(!is.na(country))
 
   cli::cli_alert_danger("The following countries lack an iso code and are dropped:
-                        {setdiff(data_shiny$name, data_shiny_filtered$name)}.")
+                        {setdiff(data_combined_raw$name, data_combined$name)}.")
 
-  readr::write_csv(data_shiny_filtered, "processed/data_shiny.csv")
+
+  # process data for shiny app -------------------------------------------------
+
+  # do we still want this?
+  # readr::write_csv(data_combined, "processed/data_combined.csv")
+
+  country_name <-
+    countrycode::codelist %>%
+    as_tibble() %>%
+    select(
+      name = country.name.en, country = iso2c
+    ) %>%
+    dplyr::mutate(country = dplyr::case_when(
+      name == "Kosovo" ~ "XK",
+      TRUE ~ country
+    ))  %>%
+    filter(!is.na(country))
+
+  # we could write it, or read it from here
+  # readr::write_csv(country_name, "../FINDCov19TrackerData/raw/country_name.csv")
+
+  country_info <-
+    readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/raw/country_info.csv", col_types = readr::cols()) %>%
+    select(-name_not_used) %>%
+    filter(!is.na(country))
+
+  # bring data to long structure
+  country <-
+    data_combined %>%
+    select(-name) %>%
+    rename(
+      cum_cases = cases,
+      cum_deaths = deaths,
+      cum_tests = tests,
+      pop = pop_100k,
+      time = date
+    ) %>%
+    select(country, time, pop, everything()) %>%
+    mutate(
+      cum_pos = 100 * cum_cases / cum_tests,
+      new_pos = 100 * new_cases / new_tests
+    ) %>%
+    tidyr::pivot_longer(
+      cum_cases:new_pos,
+      names_to = c("diff", "var"),
+      names_pattern = "(.+)_(.+)",
+      values_to = c("value_all")
+    )
+
+  region <-
+    country %>%
+    left_join(select(country_info, country, region), by = "country") %>%
+    group_by(time, unit = region, diff, var) %>%
+    summarize_at(vars(pop, value_all), sum, na.rm = TRUE) %>%
+    ungroup() %>%
+    add_column(set = "region", .before = 1)
+
+  income <-
+    country %>%
+    left_join(select(country_info, country, income), by = "country") %>%
+    group_by(time, unit = income, diff, var) %>%
+    summarize_at(vars(pop, value_all), sum, na.rm = TRUE) %>%
+    ungroup() %>%
+    add_column(set = "income", .before = 1)
+
+  all <-
+    country %>%
+    rename(unit = country) %>%
+    add_column(set = "country", .before = 1) %>%
+
+    # add_column(set = "country", .before = 1) %>%
+    bind_rows(region, income) %>%
+    # posivity rate is the same if displyed per capita
+    mutate(value_cap = if_else(var == "pos", value_all, value_all / pop)) %>%
+    select(-pop) %>%
+    tidyr::pivot_longer(
+      value_all:value_cap,
+      names_to = "ref",
+      names_pattern = "value_(.+)",
+      values_to = c("value")
+    ) %>%
+    filter(!is.na(value)) %>%
+    filter(!is.na(unit))
+
+
+  # 7 day moving averages
+  shiny_data <-
+    all %>%
+    filter(diff == "new") %>%
+    group_by(unit, var, ref) %>%
+    mutate(value = data.table::frollmean(value, 7, na.rm = T)) %>%
+    ungroup() %>%
+    bind_rows(filter(all, diff == "cum")) %>%
+    filter(!is.na(value))
+
+  all_latest_wide <-
+    shiny_data %>%
+    filter(diff == "new") %>%
+    filter(ref == "cap") %>%
+    arrange(unit, var, time) %>%
+    group_by(set, unit, diff, var, ref) %>%
+    summarize(value = value[n()]) %>%
+    ungroup() %>%
+    select(-diff, -ref) %>%
+    tidyr::pivot_wider(names_from = "var")
+
+  unit_info <-
+    all_latest_wide %>%
+    left_join(country_info, by = c("unit" = "country"))
+
+  readr::write_csv(unit_info, "../FINDCov19TrackerData/processed/unit_info.csv")
+  readr::write_csv(shiny_data, "../FINDCov19TrackerData/processed/shiny_data.csv")
+
+
 }
