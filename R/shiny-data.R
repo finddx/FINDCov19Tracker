7ddf9b1d791384ffdcaebfbf201ffbc69eacf2da
Christoph Sax <christoph.sax@gmail.com>
2020-08-18 19:28:54 +0200

    clean up country mapping

--- R/shiny-data.R
+++ R/shiny-data.R
@@ -8,88 +8,110 @@ create_shiny_data <- function() {
 
   # library(tidyverse)
 
-  # read and cobine data -------------------------------------------------------
 
-  cv_cases <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/processed/coronavirus_cases.csv",
-    col_types = readr::cols()
-  )
+  # country reference data -----------------------------------------------------
 
-  cv_tests <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/processed/coronavirus_tests.csv",
-    col_types = readr::cols()
-  )
+  # country_name <-
+  #   countrycode::codelist %>%
+  #   as_tibble() %>%
+  #   select(
+  #     name = country.name.en, country = iso2c
+  #   ) %>%
+  #   dplyr::mutate(country = dplyr::case_when(
+  #     name == "Kosovo" ~ "XK",
+  #     TRUE ~ country
+  #   ))  %>%
+  #   filter(!is.na(country))
 
-  pop <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/raw/UN_populations_2020.csv",
-    col_types = readr::cols()
-  ) %>%
-    mutate(country = recode(.data$country,
-      "Republic of Korea" = "RepublicofKorea",
-      "Congo (Brazzaville)" = "Congo",
-      # "Republic of the Congo" = Congo
-      "Democratic Republic of the Congo" = "DemocraticRepublicoftheCongo",
-      "Cote d'Ivoire" = "CotedIvoire",
-      "Cruise Ship" = "DiamondPrincessCruiseShip",
-      "Diamond Princess" = "DiamondPrincessCruiseShip",
-      "Dominican Republic" = "DominicanRepublic",
-      "El Salvador" = "ElSalvador",
-      "Equatorial Guinea" = "EquatorialGuinea",
-      "Holy See" = "HolySee",
-      "MS Zaandam" = "MSZaandam",
-      "New Zealand" = "NewZealand",
-      "North Macedonia" = "NorthMacedonia",
-      "Papua New Guinea" = "PapuaNewGuinea",
-      "Saint Kitts and Nevis" = "SaintKittsandNevis",
-      "Saint Lucia" = "SaintLucia",
-      "Saint Vincent and the Grenadines" = "SaintVincentandtheGrenadines",
-      "San Marino" = "SanMarino",
-      "Sao Tome and Principe" = "SaoTomeandPrincipe",
-      "Saudi Arabia" = "SaudiArabia",
-      "Sierra Leone" = "SierraLeone",
-      "South Sudan" = "SouthSudan",
-      "Sri Lanka" = "SriLanka",
-      "Taiwan*" = "Taiwan",
-      "Trinidad and Tobago" = "TrinidadandTobago",
-      "United Arab Emirates" = "UnitedArabEmirates",
-      "United Kingdom" = "UnitedKingdom",
-      "Western Sahara" = "WesternSahara",
-      "Gambia" = "TheGambia",
-      "Antigua and Barbuda" = "AntiguaandBarbuda",
-      "Bosnia and Herzegovina" = "BosniaandHerzegovina",
-      "Burkina Faso" = "BurkinaFaso",
-      "Central African Republic" = "CentralAfricanRepublic",
-      "Costa Rica" = "CostaRica",
-      "Bahamas" = "TheBahamas",
-      "Cabo Verde" = "CapeVerde",
-      "Syria" = "SyrianArabRepublic",
-      "Timor-Leste" = "EastTimor",
-      "South Africa" = "SouthAfrica",
-      "East Timor" = "EastTimor",
-      "West Bank and Gaza" = "occupiedPalestinianterritory",
-      "Guinea-Bissau" = "GuineaBissau",
-      "Tanzania, United Republic of" = "UnitedRepublicofTanzania",
-      "Moldova, Republic of" = "RepublicofMoldova",
-      " " = "NA",
-      "United States of America" = "US",
-      "Bolivia (Plurinational State of)" = "Bolivia",
-      "Brunei Darussalam" = "Brunei",
-      "Côte d'Ivoire" = "CotedIvoire",
-      "Iran (Islamic Republic of)" = "Iran",
-      "Lao People's Democratic Republic" = "Laos",
-      "Republic of Moldova" = "Moldova",
-      "State of Palestine" = "occupiedPalestinianterritory",
-      "Russian Federation" = "Russia",
-      "Syrian Arab Republic" = "SyrianArabRepublic",
-      "China, Taiwan Province of China" = "Taiwan",
-      "United Republic of Tanzania" = "Tanzania",
-      "Venezuela (Bolivarian Republic of)" = "Venezuela",
-      "Viet Nam" = "Vietnam"
-    ))
+  # we could write it, or read it from here
+  # readr::write_csv(country_name, "../FINDCov19TrackerData/raw/country_name.csv")
 
+
+  # read data ------------------------------------------------------------------
+
+  # regex matching table
   iso_country <-
     countrycode::codelist %>%
     as_tibble() %>%
     select(
       regex = country.name.en.regex, country = iso2c
     )
+
+  cv_cases_raw <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/processed/coronavirus_cases.csv",
+    col_types = readr::cols()
+  )
+
+  cv_tests_raw <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/processed/coronavirus_tests.csv",
+    col_types = readr::cols()
+  )
+
+  pop_raw <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/raw/UN_populations_2020.csv",
+    col_types = readr::cols()
+  )
+
+  country_info <-
+    readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/raw/country_info.csv", col_types = readr::cols()) %>%
+    select(-name_not_used) %>%
+    filter(!is.na(country_iso))
+
+
+  # use clean identifier (iso2c) -----------------------------------------------
+
+  cv_cases <-
+    cv_cases_raw %>%
+    rename(name = country) %>%
+    fuzzyjoin::regex_left_join(iso_country, by = c("name" = "regex"), ignore_case = TRUE) %>%
+    mutate(country = case_when(
+      name == "Kosovo" ~ "XK",
+      name == "SouthAfrica" ~ "ZA",
+      name == "CentralAfricanRepublic" ~ "CF",
+      name == "DominicanRepublic" ~ "DO",
+      name == "SaintLucia" ~ "LC",
+      name == "WesternSahara" ~ "EH",
+      # name == "UnitedRepublicofTanzania" ~ "TZ",
+      name == "RepublicofKorea" ~ "KR",
+      # name == "LaoPeople'sDemocraticRepublic" ~ "LA",
+      TRUE ~ country
+    )) %>%
+    # drop ships
+    filter(!(name %in% c("DiamondPrincessCruiseShip", "MSZaandam"))) %>%
+    select(-regex) %>%
+    relocate(country)
+
+  um <- unique(filter(cv_cases, is.na(country))$name)
+  if (length(um) > 0) cli::cli_alert_warning("Unmatched countries in 'cv_cases': {um}")
+
+  cv_tests <-
+    cv_tests_raw %>%
+    rename(name = country) %>%
+    fuzzyjoin::regex_left_join(iso_country, by = c("name" = "regex"), ignore_case = TRUE) %>%
+    mutate(country = case_when(
+      name == "Kosovo" ~ "XK",
+      TRUE ~ country
+    )) %>%
+    # drop non countries
+    filter(!(name %in% c("Scotland"))) %>%
+    select(-regex) %>%
+    relocate(country)
+
+  um <- unique(filter(cv_tests, is.na(country))$name)
+  if (length(um) > 0) cli::cli_alert_warning("Unmatched countries in 'cv_tests': {um}")
+
+  pop <-
+    pop_raw %>%
+    rename(name = country) %>%
+    fuzzyjoin::regex_left_join(iso_country, by = c("name" = "regex"), ignore_case = TRUE) %>%
+    mutate(country = case_when(
+      name == "Kosovo" ~ "XK",
+      TRUE ~ country
+    )) %>%
+    # drop non countries
+    filter(!(name %in% c("Channel Islands"))) %>%
+    select(-regex) %>%
+    relocate(country)
+
+  um <- unique(filter(pop, is.na(country))$name)
+  if (length(um) > 0) cli::cli_alert_warning("Unmatched countries in 'pop': {um}")
 
   # check all jhu country names have corresponding country data
   "%ni%" <- Negate("%in%")
@@ -101,13 +123,15 @@ create_shiny_data <- function() {
           countries: {countries_without_population}.", wrap = TRUE)
   }
 
-  data_combined_raw <-
-    cv_cases %>%
-    left_join(cv_tests, by = c("country" = "jhu_ID", "date" = "date")) %>%
-    left_join(pop) %>%
+  # combining data -------------------------------------------------------------
+
+  data_combined <-
+    select(cv_cases, -name) %>%
+    full_join(select(cv_tests, -name), by = c("country", "date")) %>%
+    left_join(pop, by = "country") %>%
     mutate(pop_100k = population / 100000) %>%
     select(
-      name = country,
+      country,
       date,
       cases,
       new_cases,
@@ -116,62 +140,16 @@ create_shiny_data <- function() {
       tests = tests_cumulative,
       new_tests,
       pop_100k
-    ) %>%
-    fuzzyjoin::regex_left_join(iso_country, by = c("name" = "regex"), ignore_case = TRUE) %>%
-    mutate(country = case_when(
-      name == "Kosovo" ~ "XK",
-      name == "SouthAfrica" ~ "ZA",
-      name == "CentralAfricanRepublic" ~ "CF",
-      name == "DominicanRepublic" ~ "DO",
-      name == "SaintLucia" ~ "LC",
-      name == "WesternSahara" ~ "EH",
-      name == "UnitedRepublicofTanzania" ~ "TZ",
-      name == "RepublicofKorea" ~ "KR",
-      TRUE ~ country
-    )) %>%
-    # drop ships
-    filter(!(name %in% c("DiamondPrincessCruiseShip", "MSZaandam"))) %>%
-    # 1 Diamond Princess Cruise Ship
-    # 2 MS Zaandam
-    select(-regex) %>%
-    select(name, country, everything())
+    )
 
-  # filter data which no iso_code match
-  data_combined <- data_combined_raw %>%
-    filter(!is.na(country))
-
-  cli::cli_alert_danger("The following countries lack an iso code and are dropped:
-                        {setdiff(data_combined_raw$name, data_combined$name)}.")
+  um <- unique(filter(cv_tests, is.na(country))$name)
+  if (length(um) > 0) cli::cli_alert_danger("Some missing countries in 'data_combined'")
 
 
-  # process data for shiny app -------------------------------------------------
-
-  # do we still want this?
-  # readr::write_csv(data_combined, "processed/data_combined.csv")
-
-  country_name <-
-    countrycode::codelist %>%
-    as_tibble() %>%
-    select(
-      name = country.name.en, country = iso2c
-    ) %>%
-    dplyr::mutate(country = dplyr::case_when(
-      name == "Kosovo" ~ "XK",
-      TRUE ~ country
-    ))  %>%
-    filter(!is.na(country))
-
-  # we could write it, or read it from here
-  # readr::write_csv(country_name, "../FINDCov19TrackerData/raw/country_name.csv")
-
-  country_info <-
-    readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/data/master/raw/country_info.csv", col_types = readr::cols()) %>%
-    select(-name_not_used) %>%
-    filter(!is.na(country_iso))
+  # calculations ---------------------------------------------------------------
 
   data_country <-
     data_combined %>%
-    select(-name) %>%
     # prefix cummulative vars
     rename(cum_cases = cases, cum_deaths = deaths, cum_tests = tests, time = date) %>%
     # keep orginal data in separate columns
@@ -259,6 +237,9 @@ create_shiny_data <- function() {
     select(-c(cum_cases, new_cases, cum_deaths, new_deaths, cum_tests, new_tests)) %>%
     arrange(time, set, unit)
 
+
+  # summary table --------------------------------------------------------------
+
   latest_test_date <-
     data_all %>%
     filter(set == "country") %>%
@@ -283,10 +264,11 @@ create_shiny_data <- function() {
     left_join(country_info, by = c("unit" = "country_iso")) %>%
     left_join(latest_test_date, by = "unit")
 
+
+  # writing data ---------------------------------------------------------------
+
   readr::write_csv(unit_info, "processed/unit_info.csv")
   readr::write_csv(data_all, "processed/data_all.csv")
-
-  writeLines(jsonlite::toJSON(data_all), "processed/data_all.json")
-
+  jsonlite::stream_out(data_all, file("processed/data_all.json"))
 
 }
