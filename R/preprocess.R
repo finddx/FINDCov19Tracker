c0fc337dad94371582b80d632d7fff3bde164494
pat-s <patrick.schratz@gmail.com>
2020-12-24 09:24:53 +0100

    fetch: enable Afghanistan and Argentina again

--- R/preprocess.R
+++ R/preprocess.R
@@ -11,7 +11,8 @@ fetch_test_data <- function() {
   info <- readr::read_csv("https://raw.githubusercontent.com/dsbbfinddx/FINDCov19TrackerData/master/resources/countries-urls.csv") # nolint
   # info <- readr::read_csv("/Users/pjs/git/clients/find-1/FINDCov19TrackerData/resources/countries-urls.csv") # nolint
   # info <- info[-c(44, 127), ]
-  info <- info[c(47), ]
+  # info <- info[c(47), ]
+  info <- info[c(1,6), ]
 
   # info <- info %>%
   #   dplyr::filter(!is.na(type), type != "Selenium")
