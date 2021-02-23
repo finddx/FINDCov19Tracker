d763d18aff791cdc86b763e56ee41314485151d0
angelicambg <angelicambg@gmail.com>
2021-02-23 17:01:51 +0100

    closed #31 `test_cumulative` is set to 0 to see easily the automated countries with error.

--- R/countries.R
+++ R/countries.R
@@ -1,8 +1,14 @@
 #' Find and write countries which need manual processing
 #' @description
-#'  This functions reads the list of [automated countries](https://github.com/dsbbfinddx/FINDCov19TrackerData/blob/master/manual/countries-urls.csv) via
-#' Selenium and R fetch functions and calculates which countries still need
-#' manual processing.
+#'  This functions reads the file with errors for the current date in the folder [issues](https://github.com/dsbbfinddx/FINDCov19TrackerData/tree/master/issues),
+#'  and generates a template for the current day in [manual/need-processing/](https://github.com/dsbbfinddx/FINDCov19TrackerData/tree/master/manual/need-processing).
+#'
+#' This process updates the file `-need-manual-processing.csv` *only* for the current day.
+#' To see if there are errors for past dates check the folder [issues](https://github.com/dsbbfinddx/FINDCov19TrackerData/tree/master/issues)
+#'
+#' For the automated countries with error `test_cumulative` is set to 0 in the templates [manual/need-processing/](https://github.com/dsbbfinddx/FINDCov19TrackerData/tree/master/manual/need-processing).
+#'
+#'
 #' @importFrom readr read_csv write_csv
 #' @importFrom dplyr pull filter rename
 #' @importFrom tibble add_column
@@ -49,16 +55,21 @@ calc_manual_countries <- function() {
 
   # only keep countries which need manual processing (including their
   # source URLS)
-  countries_manual_csv <- countries_error %>%
-    dplyr::left_join(countries_all) %>%
-    tibble::add_column(
-      source = "manually"
-    ) %>%
-    dplyr::relocate(
-      country, tests_cumulative, new_tests,
-      tests_cumulative_corrected, new_tests_corrected,
-      date, source, status, url
-    )
+countries_manual_csv <- countries_error %>%
+  dplyr::left_join(countries_all) %>%
+  tibble::add_column(
+    source = "manually"
+  ) %>%
+  dplyr::relocate(
+    country, tests_cumulative, new_tests,
+    tests_cumulative_corrected, new_tests_corrected,
+    date, source, status, url
+  ) %>%
+  dplyr::mutate(tests_cumulative = if_else(
+    status == "automated" & is.na(tests_cumulative),
+    0,
+    tests_cumulative
+  ))
 
   # write csv
   readr::write_csv(
