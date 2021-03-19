992a0354c894ca12fc9c683a7fa395e35312a9a0
angelicambg <38508957+angelicambg@users.noreply.github.com>
2021-03-19 21:37:06 +0100

    typo after removing Argentina from fetch

--- R/preprocess.R
+++ R/preprocess.R
@@ -12,7 +12,7 @@ fetch_test_data <- function() {
   # info <- readr::read_csv("/Users/pjs/git/clients/find-1/FINDCov19TrackerData/resources/countries-urls.csv") # nolint
   # info <- info[-c(44, 127), ]
   # info <- info[c(47), ]
-  info <- info[info$country == "Afghanistan"]
+  info <- info[info$country == "Afghanistan",]
   # info <- info %>%
   #   dplyr::filter(!is.na(type), type != "Selenium")
 
